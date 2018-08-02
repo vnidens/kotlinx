653cc51a78aef8c6363be16bd7d2851b8dfaf71c
Victor Nidens <victor.nidens@gmail.com>
2018-08-03 00:56:25 +0300

    Added documentation for map delegates

--- src/main/kotlin/com/vnidens/kotlinx/collections/MapDelegate.kt
+++ src/main/kotlin/com/vnidens/kotlinx/collections/MapDelegate.kt
@@ -4,6 +4,12 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+/**
+ * The delegate allows to use custom key with delegated immutable map for a class property
+ *
+ * @param key Key used for delegated value in the map
+ * @param map Backing map instance
+ */
 class MapDelegate<K, V>(
         private val key: K,
         private val map: Map<K, V>
--- src/main/kotlin/com/vnidens/kotlinx/collections/MutableMapDelegate.kt
+++ src/main/kotlin/com/vnidens/kotlinx/collections/MutableMapDelegate.kt
@@ -3,6 +3,12 @@ package com.vnidens.kotlinx.collections
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+/**
+ * The delegate allows to use custom key with delegated mutable map for a class property
+ *
+ * @param key Key used for delegated value in the map
+ * @param map Backing map instance
+ **/
 class MutableMapDelegate<K, V>(
         private val key: K,
         private val map: MutableMap<K, V>
